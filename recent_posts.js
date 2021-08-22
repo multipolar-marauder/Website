b11ba186640694232fed7b7155c50d4fd4913575
Multipolar Marauder <89056494+multipolar-marauder@users.noreply.github.com>
2021-08-21 22:31:04 -0400

    Update recent_posts.js

--- recent_posts.js
+++ recent_posts.js
@@ -1,56 +1,51 @@
 // For recent post data
 
-function one_recentpost() {
-  var title = "Why You Need A Dog";
-  var author = "&nbsp; Srivatsan Viswanathan &nbsp;";
-  var date = "&nbsp; August 8 2021";
-  var description =
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis quaerat eveniet, doloremque recusandae earum eligendi voluptates cum molestias consequatur blanditiis quia qui dolorum, consequuntur ratione inventore numqssssssssuam deserunt! Fugit labore fuga accusantium vero magnam. Fuga repudiandae incidunt dolorum nulla.";
+// Most Recent Post
+var title = "Why You Need A Dog";
+var author = "&nbsp; Srivatsan Viswanathan &nbsp;";
+var date = "&nbsp; August 8 2021";
+var description =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis quaerat eveniet, doloremque recusandae earum eligendi voluptates cum molestias consequatur blanditiis quia qui dolorum, consequuntur ratione inventore numqssssssssuam deserunt! Fugit labore fuga accusantium vero magnam. Fuga repudiandae incidunt dolorum nulla.";
 
-  document.getElementById("recent-posts-title").innerHTML = title;
-  document.getElementById("recent-posts-author").innerHTML = author;
-  document.getElementById("recent-posts-date").innerHTML = date;
-  document.getElementById("recent-posts-p-desc").innerHTML = description;
-  document
-    .getElementById("recent-posts-title")
-    .setAttribute("href", (href = "./blog-posts/why-you-need-a-dog.html"));
-}
+document.getElementById("recent-posts-title").innerHTML = title;
+document.getElementById("recent-posts-author").innerHTML = author;
+document.getElementById("recent-posts-date").innerHTML = date;
+document.getElementById("recent-posts-p-desc").innerHTML = description;
+document
+  .getElementById("recent-posts-title")
+  .setAttribute("href", (href = "./blog-posts/why-you-need-a-dog.html"));
 
-function two_recentpost() {
-  var title = "Why You Need A Cat";
-  var author = "&nbsp; Cat Lover &nbsp;";
-  var date = "&nbsp; August 6 2021 &nbsp;";
-  var description =
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis quaerat eveniet, doloremque recusandae earum eligendi voluptates cum molestias consequatur blanditiis quia qui dolorum, consequuntur ratione inventore numquam deserunt! Fugit labore fuga accusantium vero magnam. Fuga repudiandae incidunt dolorum nulla.";
+// Second Most Recent Post
+var title2 = "Why You Need A Cat";
+var author2 = "&nbsp; Cat Lover &nbsp;";
+var date2 = "&nbsp; August 6 2021 &nbsp;";
+var description2 =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis quaerat eveniet, doloremque recusandae earum eligendi voluptates cum molestias consequatur blanditiis quia qui dolorum, consequuntur ratione inventore numquam deserunt! Fugit labore fuga accusantium vero magnam. Fuga repudiandae incidunt dolorum nulla.";
 
-  document.getElementById("recent-posts-title2").innerHTML = title;
-  document.getElementById("recent-posts-author2").innerHTML = author;
-  document.getElementById("recent-posts-date2").innerHTML = date;
-  document.getElementById("recent-posts-p-desc2").innerHTML = description;
-  document
-    .getElementById("recent-posts-title2")
-    .setAttribute("href", (href = "./blog-posts/why-you-need-a-cat.html"));
-}
+document.getElementById("recent-posts-title2").innerHTML = title2;
+document.getElementById("recent-posts-author2").innerHTML = author2;
+document.getElementById("recent-posts-date2").innerHTML = date2;
+document.getElementById("recent-posts-p-desc2").innerHTML = description2;
+document
+  .getElementById("recent-posts-title2")
+  .setAttribute("href", (href = "./blog-posts/why-you-need-a-cat.html"));
 
-function three_recentpost() {
-  var title = "Why Pets Are Good For You";
-  var author = "&nbsp; Srivatsan Viswanathan &nbsp;";
-  var date = "&nbsp; August 4 2021 &nbsp;";
-  var description =
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis quaerat eveniet, doloremque recusandae earum eligendi voluptates cum molestias consequatur blanditiis quia qui dolorum, consequuntur ratione inventore numquam deserunt! Fugit labore fuga accusantium vero magnam. Fuga repudiandae incidunt dolorum nulla.";
+//Third Most Recent Post
+var title3 = "Why Pets Are Good For You";
+var author3 = "&nbsp; Srivatsan Viswanathan &nbsp;";
+var date3 = "&nbsp; August 4 2021 &nbsp;";
+var description3 =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis quaerat eveniet, doloremque recusandae earum eligendi voluptates cum molestias consequatur blanditiis quia qui dolorum, consequuntur ratione inventore numquam deserunt! Fugit labore fuga accusantium vero magnam. Fuga repudiandae incidunt dolorum nulla.";
 
-  document.getElementById("recent-posts-title3").innerHTML = title;
-  document.getElementById("recent-posts-author3").innerHTML = author;
-  document.getElementById("recent-posts-date3").innerHTML = date;
-  document.getElementById("recent-posts-p-desc3").innerHTML = description;
-  document
-    .getElementById("recent-posts-title3")
-    .setAttribute(
-      "href",
-      (href = "./blog-posts/why-pets-are-good-for-you.html")
-    );
-}
+document.getElementById("recent-posts-title3").innerHTML = title3;
+document.getElementById("recent-posts-author3").innerHTML = author3;
+document.getElementById("recent-posts-date3").innerHTML = date3;
+document.getElementById("recent-posts-p-desc3").innerHTML = description3;
+document
+  .getElementById("recent-posts-title3")
+  .setAttribute("href", (href = "./blog-posts/why-pets-are-good-for-you.html"));
 
+//Images
 document.getElementById("recent-one-image").src = "./Images/dog.jpg";
 document.getElementById("recent-two-image").src = "./Images/cat.jpg";
 document.getElementById("recent-three-image").src = "./Images/pets.jpg";
