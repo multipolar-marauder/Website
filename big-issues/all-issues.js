3a97e9d7d9575b74e6e4a119dc90f4f844015dd7
Srivatsan <sriv.viswanathan@gmail.com>
2021-08-25 14:24:07 -0400

    JS updates

--- big-issues/all-issues.js
+++ big-issues/all-issues.js
@@ -10,6 +10,8 @@ const abortion = {
   name: "Abortion",
   pro_percent: 83,
   con_percent: 17,
+  link: "abortion.html",
+  image: "abortion.jpg",
   desc: "Abortion involves terminating a pregnancy by the removing or expelling of a fetus or embryo from the uterus. It is considered one of the most controversial issues due to its legal cultural and religious implications. <br /> <br /> On one side, you have pro-life groups that believe that abortion is violating the right to life of a child, and seek to create legal restrictions on abortion or even completely prohibt it. In contrast, pro-choice groups argue that a woman has certain reproductive rights and a right to bodily autonomy. <br /> <br /> Abortion is currently legal in both Canada and the United States, with the former having no abortion laws at all. Roe v Wade in the United States had ruled in favor for abortion rights, however several states have enacted legislature that attempts to curtail the access and ease of going through an abortion.",
 };
 
@@ -17,6 +19,8 @@ const affirmative_action = {
   name: "Affirmative Action",
   pro_percent: 39,
   con_percent: 61,
+  link: "affirmative-action.html",
+  image: "affirmative_action.jpg",
   desc: "Affirmative Action programs are created by the government with the purpose of increasing the enrollment and acceptance of various populations that are deemed to be disfranchised.",
 };
 
@@ -24,6 +28,8 @@ const automation = {
   name: "Automation",
   pro_percent: 89,
   con_percent: 11,
+  link: "automation.html",
+  image: "automation.jpg",
   desc: "With the advance of technology, in particular AI, occupations that once required humans are now able to be substituted with machines. During the industrial revolution and beyond, repetitive actions were increasingly becoming automated by machines. Workers shifted towards the service sectors and occupations that still required humans.",
 };
 
@@ -31,6 +37,8 @@ const barack_obama = {
   name: "Barack Obama",
   pro_percent: 40,
   con_percent: 60,
+  link: "barack-obama.html",
+  image: "barack_obama.jpg",
   desc: "Barack Obama was the 44th President of the United States who served from 2008-2016. Being the first Black President, Obama broke racial barriers and sought to create a new legacy for the African-American Race, who had been historically oppressed by the White majority. <br /> <br /> Supporters of Obama credit him for the Affordable Care Act, which sought to increase healthcare access across America, and put regulations on health insurance companies from denying patients with pre-existing conditions. His stimulus package was very important during the 2008 recession and benefited many Americans who were unemployed. He is also credited for ordering Operation Neptune-Spear, the military mission that killed Al-Qaeda leader Osama Bin Laden, the mastermind behind the September 11 attacks which killed nearly 3000 Americans. <br /> <br /> Critics of Obama have blamed him for increasing the deficit of the U.S federal government and expanding federal programs such as Common Core. He has also been criticised for his role in the Arab Spring, which toppled dictators in Egypt, Libya while failing to do so in Syria. The desicions of the Obama adminstration are said to have lead to the rise of ISIS, a brutal Islamic jihadist organization that launched terrorist attacks all across the world. Some of his own party members have criticized him for being too soft on issues like gun control, for not keeping his promises of shutting down Guantanamo Bay, and for his controversial Drone Strike program which killed many civilians.",
 };
 
@@ -38,6 +46,8 @@ const canada = {
   name: "Canada",
   pro_percent: 86,
   con_percent: 14,
+  link: "canada.html",
+  image: "canada.jpg",
   desc: "Canada has generally been regarded as a tolerant democratic country that has prioritized human rights and provides its citizens with a high quality of life. It consistently ranks among the top countries for its development and has an abundance of natural resources, having the largest freshwater reserve in the world. Canada is renowned for its diversity, and is one of the few countries that promote Multiculturalism as a positive value. For those reasons it has been a top destination for many immigrant groups, and has taken in many refugees from around the world. <br /> <br /> Although Canada has been a leading voice in human rights concern, it has been criticized for it’s hypocritical stance  given Canada’s historically poor treatment of indigenous peoples, activists have increasingly taken up the view that the country is an illegitimate settler state built on stolen land, and as such, should not be viewed positively today. Canada’s current treatment of First Nations has been criticised as inhumane and paternalistic, denying them their self-determination. Canada has also been criticised for it’s high emission of fossil fuels and over dependence on the oil industry.",
 };
 
@@ -45,12 +55,16 @@ const capitalism = {
   name: "Capitalism",
   pro_percent: 84,
   con_percent: 16,
+  link: "capitalism.html",
+  image: "abortion.jpg",
   desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque, fuga rem dolores debitis sapiente nemo officiis tempore reprehenderit magnam eos, et eum! Qui vero illo esse iure voluptate, consequatur, soluta asperiores veniam exercitationem illum hic. Repellat consectetur magni corrupti voluptas optio illum veniam blanditiis numquam vel delectus! Sit unde assumenda quis fugit cupiditate non incidunt vero ipsa voluptates nemo. Itaque omnis ipsa nemo, minima architecto nam dicta laborum veniam illo blanditiis fugit assumenda illum ex. Eveniet nulla tenetur facere debitis est nobis quia sapiente quam ea. Dolor ex soluta molestias nisi quisquam, molestiae et, facere explicabo iure, vero hic nesciunt!",
 };
 const carbon_tax = {
   name: "Carbon Tax",
   pro_percent: 68,
   con_percent: 32,
+  link: "carbon-tax.html",
+  image: "carbon_tax.jpg",
   desc: "The carbon tax is a tax levied by the government on carbon emissions. It is viewed as a way to incentivise private industries to move away from fossil fuels as their primary source of energy, and thus lessen the future impact of climate change. Many provinces have their own carbon pricing schemes, but in 2019, the federal government imposed their own carbon tax on the remaining provinces that had not adopted one. The provinces opposed this on the grounds that it infringed on their sovereignty, but they were defeated in the Supreme Court. The proceeds from the federal carbon tax are returned to the provinces, to spend as they see fit. Many use the funds to invest in green energy sources, or to give income-based tax rebates. <br /> <br /> Proponents of the carbon tax cite research showing it is effective in lowering carbon emissions, the harm caused by climate change due to the unrestrained emissions of greenhouse gases, and the tax-rebates offered by the revenue the tax brings in. Opponents of the carbon tax believe that it is an unfavour tax on consumers, as it often results in price increases for certain goods and services, and it stifles industries that still primarily rely on fossil fuels.",
 };
 
@@ -58,6 +72,8 @@ const china = {
   name: "China",
   pro_percent: 15,
   con_percent: 85,
+  link: "china.html",
+  image: "china.jpg",
   desc: "China is currently one of the leading powers of the world, and an economic powerhouse with a population of over 1.4 billion people. The country has a very rich history and historically has been one of the greatest civilizations of this planet, through its rich resources and scientific innovations. In the late 40’s, China was taken over by the Communists who eventually moved away from pure socialism to a State Capitalism. The opening of markets created the largest period of economic growth for China, and it currently challenges the U.S in terms of economic might. <br /> <br /> Supporters of China have credited it as an alternative to American hegemony, as it avoids the use of military intervention, and prefers to pursue trade partnerships and economic investment. China’s efficiency has been praised as a political model that challenges the traditional dominance of liberal democracies who have been criticised for destabilizing countries in the Global South under the pretext of human rights. <br /> <br /> Critics of China have pointed out it’s extremely poor record on human rights, as it has abused workers and pays slave wages to keep up it’s economic output. It’s treatment of the Uighurs minority has been labelled as a genocide by many countries around the world for imprisoning their population in concentration camps and for delibrating reducing their population through abortion and forced steralization. China has also been accused of the “Debt Trap” where it gives exorbitant loans to struggling countries, and then takes control of it’s infracture in return for loan forgiveness. This practice has been deemed by critics as a force of imperialism.",
 };
 
@@ -65,6 +81,8 @@ const climate_change_action = {
   name: "Climate Change Action",
   pro_percent: 91,
   con_percent: 9,
+  link: "climate-change-action.html",
+  image: "climate_change.jpg",
   desc: "The debate around climate change revolves around environmental changes caused by anthropogenic activities. In the last couple decades, the globe has increased it’s temperatures at an alarming rate. The debate revolves around how much of this climate change can be attributed to human activity, and how much effort countries should take to curb it. <br /> <br /> Proponents of Climate Change action argue that the government needs to radically change it’s economic policies to promote green energy and reduce carbon emissions. They believe that corporations and non-renewable energy production like coal and oil are primarily responsible for global warming, and it will lead with many ecological problems and natural disasters. <br /> <br /> Opponents of Climate Change action argue that climate change is mostly a naturally occurring cycle, and even if humans do contribute to it, there is unnecessary alarmism over what needs to be done. They believe that economic production and growth should not be hurt by climate change policies and government crackdown on energy production will lead to major job loss for many blue collar workers employed in these industries.",
 };
 
@@ -72,6 +90,8 @@ const critical_race_theory = {
   name: "Critical Race Theory",
   pro_percent: 35,
   con_percent: 65,
+  link: "critical-race-theory.html",
+  image: "critical_race_theory.jpg",
   desc: "Critical Race theory is a political and philosophical framework that stresses the idea that the concept of “Race” has no biological basis and was solely created by the White majority in order to exploit other races of color, more specifically Blacks. The theory dictates that the United States and other Western countries are inherently racist institutions that are upheld by racist laws that keep Whites in powe.",
 };
 
@@ -356,131 +376,59 @@ const world_trade_organization = {
 };
 
 var issues = [
-  [abortion.name, abortion.pro_percent, abortion.con_percent],
-  [
-    affirmative_action.name,
-    affirmative_action.pro_percent,
-    affirmative_action.con_percent,
-  ],
-  [automation.name, automation.pro_percent, automation.con_percent],
-  [barack_obama.name, barack_obama.pro_percent, barack_obama.con_percent],
-  [canada.name, canada.pro_percent, canada.con_percent],
-  [capitalism.name, capitalism.pro_percent, capitalism.con_percent],
-  [carbon_tax.name, carbon_tax.pro_percent, carbon_tax.con_percent],
-  [china.name, china.pro_percent, china.con_percent],
-  [
-    climate_change_action.name,
-    climate_change_action.pro_percent,
-    climate_change_action.con_percent,
-  ],
-  [
-    critical_race_theory.name,
-    critical_race_theory.pro_percent,
-    critical_race_theory.con_percent,
-  ],
-  [death_penalty.name, death_penalty.pro_percent, death_penalty.con_percent],
-  [
-    defund_the_police.name,
-    defund_the_police.pro_percent,
-    defund_the_police.con_percent,
-  ],
-  [donald_trump.name, donald_trump.pro_percent, donald_trump.con_percent],
-  [doug_ford.name, doug_ford.pro_percent, doug_ford.con_percent],
-  [drone_strikes.name, drone_strikes.pro_percent, drone_strikes.con_percent],
-  [
-    drug_legalization.name,
-    drug_legalization.pro_percent,
-    drug_legalization.con_percent,
-  ],
-  [
-    enthusiastic_consent_laws.name,
-    enthusiastic_consent_laws.pro_percent,
-    enthusiastic_consent_laws.con_percent,
-  ],
-  [estate_tax.name, estate_tax.pro_percent, estate_tax.con_percent],
-  [european_union.name, european_union.pro_percent, european_union.con_percent],
-  [euthanasia.name, euthanasia.pro_percent, euthanasia.con_percent],
-  [flat_tax.name, flat_tax.pro_percent, flat_tax.con_percent],
-  [george_w_bush.name, george_w_bush.pro_percent, george_w_bush.con_percent],
-  [
-    hate_speech_law.name,
-    hate_speech_law.pro_percent,
-    hate_speech_law.con_percent,
-  ],
-  [home_schooling.name, home_schooling.pro_percent, home_schooling.con_percent],
-  [indian_act.name, indian_act.pro_percent, indian_act.con_percent],
-  [
-    israel_right_to_exist.name,
-    israel_right_to_exist.pro_percent,
-    israel_right_to_exist.con_percent,
-  ],
-  [justin_trudaeu.name, justin_trudaeu.pro_percent, justin_trudaeu.con_percent],
-  [labor_unions.name, labor_unions.pro_percent, labor_unions.con_percent],
-  [
-    late_term_abortions.name,
-    late_term_abortions.pro_percent,
-    late_term_abortions.con_percent,
-  ],
-  [lgbtq_rights.name, lgbtq_rights.pro_percent, lgbtq_rights.con_percent],
-  [
-    military_intervention.name,
-    military_intervention.pro_percent,
-    military_intervention.con_percent,
-  ],
-  [
-    military_spending.name,
-    military_spending.pro_percent,
-    military_spending.con_percent,
-  ],
-  [minimum_wage.name, minimum_wage.pro_percent, minimum_wage.con_percent],
-  [nato.name, nato.pro_percent, nato.con_percent],
-  [
-    palestine_right_to_exist.name,
-    palestine_right_to_exist.pro_percent,
-    palestine_right_to_exist.con_percent,
-  ],
-  [patriotism.name, patriotism.progressive_tax, patriotism.con_percent],
-  [
-    progressive_tax.name,
-    progressive_tax.pro_percent,
-    progressive_tax.con_percent,
-  ],
-  [
-    right_to_flag_burning.name,
-    right_to_flag_burning.pro_percent,
-    right_to_flag_burning.con_percent,
-  ],
-  [russia.name, russia.pro_percent, russia.con_percent],
-  [term_limits.name, term_limits.pro_percent, term_limits.con_percent],
-  [trans_sports.name, trans_sports.pro_percent, trans_sports.con_percent],
-  [united_nations.name, united_nations.pro_percent, united_nations.con_percent],
-  [united_states.name, united_states.pro_percent, united_states.con_percent],
-  [
-    universal_basic_income.name,
-    universal_basic_income.pro_percent,
-    universal_basic_income.con_percent,
-  ],
-  [
-    vaccine_passports.name,
-    vaccine_passports.pro_percent,
-    vaccine_passports.con_percent,
-  ],
-  [
-    war_in_afghanistan.name,
-    war_in_afghanistan.pro_percent,
-    war_in_afghanistan.con_percent,
-  ],
-  [war_in_iraq.name, war_in_iraq.pro_percent, war_in_iraq.con_percent],
-  [war_on_terror.name, war_on_terror.pro_percent, war_on_terror.con_percent],
-  [welfare.name, welfare.pro_percent, welfare.con_percent],
-  [
-    world_trade_organization.name,
-    world_trade_organization.pro_percent,
-    world_trade_organization.con_percent,
-  ],
+  abortion,
+  affirmative_action,
+  automation,
+  barack_obama,
+  canada,
+  capitalism,
+  carbon_tax,
+  china,
+  climate_change_action,
+  critical_race_theory,
+  defund_the_police,
+  death_penalty,
+  donald_trump,
+  doug_ford,
+  drone_strikes,
+  drug_legalization,
+  enthusiastic_consent_laws,
+  estate_tax,
+  european_union,
+  euthanasia,
+  flat_tax,
+  george_w_bush,
+  home_schooling,
+  hate_speech_law,
+  indian_act,
+  israel_right_to_exist,
+  justin_trudaeu,
+  lgbtq_rights,
+  labor_unions,
+  late_term_abortions,
+  military_spending,
+  military_intervention,
+  minimum_wage,
+  nato,
+  palestine_right_to_exist,
+  patriotism,
+  progressive_tax,
+  right_to_flag_burning,
+  russia,
+  term_limits,
+  trans_sports,
+  united_nations,
+  united_states,
+  universal_basic_income,
+  vaccine_passports,
+  war_in_afghanistan,
+  war_in_iraq,
+  war_on_terror,
+  welfare,
+  world_trade_organization,
 ];
 
-var sorted_issues = issues.sort();
+issues.sort((a, b) => (a.name > b.name ? 1 : -1));
 
 function call_display_issue(x, y, z) {
   display_issue(x, y, z);
--- big-issues/big-issues.js
+++ big-issues/big-issues.js
@@ -14,6 +14,7 @@ function big_issue(issue) {
   var percent2 = issue.con_percent + "%";
   var pro = "PRO";
   var con = "CON";
+  var image = issue.image;
 
   document.getElementById("issue_title").innerHTML = issue_title;
   document.getElementById("issue_desc_title").innerHTML = desc_title;
@@ -24,6 +25,8 @@ function big_issue(issue) {
   document.getElementById("vote-number-con").innerHTML = percent2;
   document.getElementById("vote-types-pro").innerHTML = pro;
   document.getElementById("vote-types-con").innerHTML = con;
+  document.getElementById("issue-image").src = "../Website Images/" + image;
+  console.log(image);
 
   document.getElementById("box-pro").style.paddingRight = padding_formula_pro(
     issue.pro_percent
@@ -58,15 +61,17 @@ function padding_formula_con(con) {
 
 function display_issue() {
   for (let i = 0; i < 50; i++) {
-    document.getElementById("issue" + i).innerHTML = sorted_issues[i][0];
+    document.getElementById("issue" + i).innerHTML = issues[i].name;
+    document.getElementById("issue" + i).href =
+      "./big-issues/" + issues[i].link;
   }
 }
 
 function display_percent() {
   for (let i = 0; i < 50; i++) {
-    if (sorted_issues[i][1] >= sorted_issues[i][2]) {
+    if (issues[i].pro_percent >= issues[i].con_percent) {
       document.getElementById("pro_or_con" + i).innerHTML =
-        " " + sorted_issues[i][1] + "%" + " pro";
+        " " + issues[i].pro_percent + "%" + " pro";
       document.getElementById("pro_or_con" + i).style.backgroundColor =
         "#66cc00";
       document.getElementById("pro_or_con" + i).style.paddingTop = "2.5%";
@@ -88,7 +93,7 @@ function display_percent() {
       }
       if (mobile.matches) {
         document.getElementById("pro_or_con" + i).innerHTML =
-          " " + sorted_issues[i][1] + "%" + " pro";
+          " " + issues[i].pro_percent + "%" + " pro";
         document.getElementById("pro_or_con" + i).style.backgroundColor =
           "#66cc00";
         document.getElementById("pro_or_con" + i).style.paddingTop = "2.5%";
@@ -101,7 +106,7 @@ function display_percent() {
       }
     } else {
       document.getElementById("pro_or_con" + i).innerHTML =
-        sorted_issues[i][2] + "%" + " con";
+        issues[i].con_percent + "%" + " con";
       document.getElementById("pro_or_con" + i).style.backgroundColor =
         "orange";
       document.getElementById("pro_or_con" + i).style.paddingTop = "2.5%";
@@ -126,7 +131,7 @@ function display_percent() {
 
       if (mobile.matches) {
         document.getElementById("pro_or_con" + i).innerHTML =
-          sorted_issues[i][2] + "%" + " con";
+          issues[i].con_percent + "%" + " con";
         document.getElementById("pro_or_con" + i).style.backgroundColor =
           "orange";
         document.getElementById("pro_or_con" + i).style.paddingTop = "2.5%";
@@ -145,9 +150,9 @@ function top_pro(array) {
   let max = 0;
   let max_issue = "Abortion";
   for (let i = 0; i < array.length; i++) {
-    if (array[i][1] >= max) {
-      max = array[i][1];
-      max_issue = array[i][0];
+    if (array[i].pro_percent >= max) {
+      max = array[i].pro_percent;
+      max_issue = array[i].name;
     }
   }
   document.getElementById("index-issue1").innerHTML = max_issue;
@@ -158,9 +163,9 @@ function top_con(array) {
   let max = 0;
   let max_issue = "Abortion";
   for (let i = 0; i < array.length; i++) {
-    if (array[i][2] >= max) {
-      max = array[i][2];
-      max_issue = array[i][0];
+    if (array[i].con_percent >= max) {
+      max = array[i].con_percent;
+      max_issue = array[i].name;
     }
   }
   document.getElementById("index-issue2").innerHTML = max_issue;
@@ -174,41 +179,38 @@ function middle(array) {
   let final_j;
   let middle_issue = "Abortion";
   for (let i = 0; i < array.length; i++) {
-    for (let j = 1; j < 3; j++) {
-      if (array[i][j] == middle_num) {
-        middle_issue = array[i][0];
-        document.getElementById("index-issue3").innerHTML = middle_issue;
-        document.getElementById("middle").innerHTML = middle_num + "% " + "pro";
-        document.getElementById("middle").style.backgroundColor = "#66cc00";
-        document.getElementById("middle").style.paddingTop = "2.5%";
-        document.getElementById("middle").style.paddingBottom = "3.5%";
-        document.getElementById("middle").style.paddingLeft = "7%";
-        document.getElementById("middle").style.paddingRight = "7%";
-        document.getElementById("middle").style.textTransform = "uppercase";
+    if (array[i].pro_percent == middle_num) {
+      middle_issue = array[i].name;
+      document.getElementById("index-issue3").innerHTML = middle_issue;
+      document.getElementById("middle").innerHTML = middle_num + "% " + "pro";
+      document.getElementById("middle").style.backgroundColor = "#66cc00";
+      document.getElementById("middle").style.paddingTop = "2.5%";
+      document.getElementById("middle").style.paddingBottom = "3.5%";
+      document.getElementById("middle").style.paddingLeft = "7%";
+      document.getElementById("middle").style.paddingRight = "7%";
+      document.getElementById("middle").style.textTransform = "uppercase";
+      document.getElementById("middle").style.borderRadius = "0px";
+      if (ipad.matches) {
         document.getElementById("middle").style.borderRadius = "0px";
-        if (ipad.matches) {
-          document.getElementById("middle").style.borderRadius = "0px";
-          document.getElementById("middle").style.paddingRight = "4%";
-          document.getElementById("middle").style.paddingLeft = "8%";
-        }
-        if (mobile.matches) {
-          document.getElementById("middle").style.borderRadius = "0px";
-          document.getElementById("middle").style.paddingRight = "7%";
-          document.getElementById("middle").style.paddingLeft = "7%";
-        }
-        return middle;
-      } else {
-        if (smallest >= Math.abs(array[i][j] - 50)) {
-          smallest = Math.abs(array[i][j] - 50);
-          smallest_issue = array[i][j];
-          smallest_result = array[i][0];
-          if (array[i][2] > array[i][1]) {
-            smallest_issue = array[i][2];
-            final_j = 2;
-          } else {
-            final_j = 1;
-            smallest_issue = array[i][1];
-          }
+        document.getElementById("middle").style.paddingRight = "4%";
+        document.getElementById("middle").style.paddingLeft = "8%";
+      }
+      if (mobile.matches) {
+        document.getElementById("middle").style.borderRadius = "0px";
+        document.getElementById("middle").style.paddingRight = "7%";
+        document.getElementById("middle").style.paddingLeft = "7%";
+      }
+      return middle;
+    } else {
+      if (smallest >= Math.abs(array[i].pro_percent - 50)) {
+        smallest = Math.abs(array[i].pro_percent - 50);
+        smallest_result = array[i].name;
+        if (array[i].con_percent > array[i].pro_percent) {
+          smallest_issue = array[i].con_percent;
+          final_j = 2;
+        } else {
+          final_j = 1;
+          smallest_issue = array[i].pro_percent;
         }
       }
     }
